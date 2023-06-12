b4a61b1d4732f77e4298d216f05cc4b69674ce80
jcbrown602 <3117301+JCBrown602@users.noreply.github.com>
2023-06-12 12:33:02 -0500

    Iterable error in calculateHandScore().

--- blackjack.js
+++ blackjack.js
@@ -3,6 +3,8 @@ const model = {
   deck: [],
   playerHand: [],
   dealerHand: [],
+  playerScore: 0,
+  dealerScore: 0,
 };
 
 // View
@@ -27,15 +29,19 @@ const view = {
   clearTable: function () {
     const playerHandElement = document.getElementById("player-hand");
     const dealerHandElement = document.getElementById("dealer-hand");
+    const playerHandH2 = document.getElementById("player-h2");
+    const dealerHandH2 = document.getElementById("dealer-h2");
     playerHandElement.innerHTML = "";
     dealerHandElement.innerHTML = "";
+    playerHandH2.innerHTML = "Player: ";
+    dealerHandH2.innerHTML = "Dealer: ";
   },
 
   updateScores: function (playerScore, dealerScore) {
-    const playerHandElement = document.getElementById("player-h2");
-    const dealerHandElement = document.getElementById("dealer-h2");
-    playerHandElement.textContent = `Player Hand: ${playerScore}`;
-    dealerHandElement.textContent = `Dealer Hand: ${dealerScore}`;
+    const playerHandH2 = document.getElementById("player-h2");
+    const dealerHandH2 = document.getElementById("dealer-h2");
+    playerHandH2.textContent = `Player Hand: ${playerScore}`;
+    dealerHandH2.textContent = `Dealer Hand: ${dealerScore}`;
   },
 };
 
@@ -45,9 +51,12 @@ const controller = {
     model.deck = getShuffledDeck();
     model.playerHand = [drawCard(), drawCard()];
     model.dealerHand = [drawCard(), drawCard()];
+    model.playerScore = this.calculateHandScore(model.playerHand);
+    model.dealerScore = this.calculateHandScore(model.dealerHand);
 
     view.clearTable();
     view.displayMessage("Welcome to Blackjack!");
+    this.checkGameStatus();
     this.displayHands();
     this.calculateScores();
 
@@ -84,8 +93,16 @@ const controller = {
     let score = 0;
     let hasAce = false;
 
+    for (const theHand of Object.keys(hand)) {
+      const suit = hand[theHand];
+      console.log(theHand, suit);
+    }
+
+    console.log(`hand is: ${hand}, and typeOf: ${JSON.stringify(hand, null, 2)}`);
+    //const handStr = JSON.stringify(hand);
     for (let card of hand) {
-      const rank = card.slice(0, -1);
+      console.log(`hand is: ${hand}, and typeOf: ${typeof(hand)}`);
+      const rank = hand.slice(0, -1);
 
       if (rank === "A") {
         hasAce = true;
@@ -104,7 +121,7 @@ const controller = {
     return score;
   },
 
-  dealCardToPlayer: function () {
+  dealCardToPlayer: function (player) {
     const card = drawCard();
     model.playerHand.push(card);
     const playerHandElement = document.getElementById("player-hand");
@@ -116,7 +133,72 @@ const controller = {
       view.displayMessage("Bust! You lose.");
       document.getElementById("hit-button").disabled = true;
       document.getElementById("stand-button").disabled = true;
+      this.dealerTurn(player);
+    } else if (model.playerScore === 21) {
+      view.displayMessage("Blackjack! You win.");
+      document.getElementById("hit-button").disabled = true;
+      document.getElementById("stand-button").disabled = true;
     }
+  },  
+  
+  dealerTurn: function (player) {
+    this.checkGameStatus();
+    model.playerScore = this.calculateHandScore(model.playerHand);
+    model.dealerScore = this.calculateHandScore(model.dealerScore);
+    if (player.score > 21 && dealer.score <= 21) {
+      view.displayMessage("Bust! You lose. Dealer wins.");
+    }
+    while (model.dealerScore < 17) {
+      const card = drawCard();
+      model.dealerHand.push(card);
+      view.renderCard(card, "dealer-hand");
+      model.dealerScore = this.calculateHandScore(model.dealerHand);
+      this.displayScores();
+    }
+
+    if (model.dealerScore > 21) {
+      view.displayMessage("Dealer busts! You win.");
+      } else if (model.dealerScore >= 17 && model.dealerScore <= 21) {
+        if (model.dealerScore > model.playerScore) {
+          view.displayMessage("Dealer wins.");
+        } else if (model.dealerScore < model.playerScore) {
+          view.displayMessage("You win.");
+        } else {
+          view.displayMessage("It's a tie.");
+        }
+      } else if (model.dealerScore === 17) {
+        model.dealerScore = this.calculateHandScore(model.dealerHand);
+        this.displayScores();
+      }
+  },
+
+  checkGameStatus: function () {
+    const playerScore = model.playerScore;
+    const dealerScore = model.dealerScore;
+    document.getElementById("hit-button").disabled = true;
+    document.getElementById("stand-button").disabled = true;
+
+    if (playerScore > 21) {
+      view.displayMessage("Bust! You lose.");
+    } else if (dealerScore > 21 && playerScore <= 21) {
+      view.displayMessage("Dealer busts! You win.");
+    } else if (playerScore === 21 && dealerScore === 21) {
+      view.displayMessage("It's a tie.");
+    } else if (playerScore === 21) {
+      view.displayMessage("Blackjack! You win.");
+    } else if (dealerScore === 21) {
+      view.displayMessage("Dealer has blackjack. You lose.");
+    } else if (playerScore >= 17 && dealerScore >= 17) {
+      if (playerScore > dealerScore) {
+        view.displayMessage("You win.");
+      } else if (playerScore < dealerScore) {
+        view.displayMessage("Dealer wins.");
+      } else {
+        view.displayMessage("It's a tie.");
+      }
+    }
+    document.getElementById("hit-button").disabled = false;
+    document.getElementById("stand-button").disabled = false;
   },
 };
 
@@ -150,5 +232,12 @@ document.getElementById("start-button").addEventListener("click", function () {
 });
 
 document.getElementById("hit-button").addEventListener("click", function () {
-  controller.dealCardToPlayer();
+  controller.dealCardToPlayer(this.player);
+});
+
+document.getElementById("stand-button").addEventListener("click", function () {
+  console.log("> Stand Button <");
+  document.getElementById("hit-button").disabled = true;
+  document.getElementById("stand-button").disabled = true;
+  controller.dealerTurn(this.player);
 });
